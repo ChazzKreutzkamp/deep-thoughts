f428a47f2627c22ceaf147b1724926ac1df751b9
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2021-01-10 20:40:59 -0700

    reactions and thoughts added.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,7 +39,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-            return user;
+            const token = signToken(user);
+
+            return { token, user };
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
